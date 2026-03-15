068bbea1f2e5063da0d3a9c8bb489a77b1e1e997
DHRUV12312 <146573408+DHRUV12312@users.noreply.github.com>
2026-03-15 21:41:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,73 +1,83 @@
 import streamlit as st
+import pandas as pd
+import pickle
 import requests
 
-# TMDB API key
+# your TMDB api key
 API_KEY = "579034084262bc42447e9861942396dd"
 
-# sample movie list
-movies = [
-    "Avatar",
-    "Titanic",
-    "The Dark Knight",
-    "Avengers",
-    "Inception",
-    "Interstellar",
-    "Joker",
-    "Frozen",
-    "Spider-Man",
-    "Iron Man"
-]
+# load files
+movies = pd.read_csv("movies.csv")
+similarity = pickle.load(open("similarity.pkl", "rb"))
 
 
-# function to fetch movie poster
-def fetch_poster(movie):
+# fetch poster using API
+def fetch_poster(movie_id):
 
-    url = "https://api.themoviedb.org/3/search/movie"
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key={API_KEY}&language=en-US"
+    data = requests.get(url).json()
 
-    params = {
-        "api_key": API_KEY,
-        "query": movie
-    }
+    poster_path = data["poster_path"]
 
-    data = requests.get(url, params=params).json()
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
 
-    if data["results"]:
-        poster_path = data["results"][0]["poster_path"]
-        return "https://image.tmdb.org/t/p/w500/" + poster_path
-
-    return None
+    return full_path
 
 
-# recommend movies
+# recommend function
 def recommend(movie):
 
-    recommendations = []
+    movie_index = movies[movies['title'] == movie].index[0]
 
-    for m in movies:
-        if m != movie:
-            recommendations.append(m)
+    distances = similarity[movie_index]
 
-    return recommendations[:5]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_posters = []
+
+    for i in movies_list:
+
+        movie_id = movies.iloc[i[0]].movie_id
+
+        recommended_movies.append(movies.iloc[i[0]].title)
+
+        recommended_posters.append(fetch_poster(movie_id))
+
+    return recommended_movies, recommended_posters
 
 
-# Streamlit UI
+# UI
 st.title("🎬 Movie Recommendation System")
 
-selected_movie = st.selectbox("Choose a movie", movies)
+selected_movie_name = st.selectbox(
+    "Select a movie",
+    movies['title'].values
+)
+
 
 if st.button("Recommend"):
 
-    names = recommend(selected_movie)
+    names, posters = recommend(selected_movie_name)
 
-    cols = st.columns(5)
+    col1, col2, col3, col4, col5 = st.columns(5)
 
-    for i in range(len(names)):
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
 
-        with cols[i]:
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
 
-            poster = fetch_poster(names[i])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
 
-            if poster:
-                st.image(poster)
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
 
-            st.write(names[i])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])

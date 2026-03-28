b9cdc47b880eed66cd58e21a8bb740a1253bb24d
DHRUV12312 <146573408+DHRUV12312@users.noreply.github.com>
2026-03-28 10:54:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import streamlit as st
 import requests
-import kagglehub
-
-
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-# Download latest version
-path = kagglehub.dataset_download("gsimonx37/letterboxd")
+movies = pd.read_csv("movies.csv")
 
-print("Path to dataset files:", path)
+movies['genres'] = movies['genres'].fillna('')
+movies['genres'] = movies['genres'].str.replace('|', ' ', regex=False)
+
+cv = CountVectorizer()
+matrix = cv.fit_transform(movies['genres'])
+similarity = cosine_similarity(matrix)
+
 API_KEY = "579034084262bc42447e9861942396dd"
 
 def fetch_poster(movie):
@@ -19,49 +21,22 @@ def fetch_poster(movie):
     poster_path = data["results"][0]["poster_path"]
     return "https://image.tmdb.org/t/p/w500/" + poster_path
 
-
-
-# Clean data
-movies['genres'] = movies['genres'].fillna('')
-movies['genres'] = movies['genres'].str.replace('|', ' ', regex=False)
-
-# Vectorization
-cv = CountVectorizer()
-matrix = cv.fit_transform(movies['genres'])
-
-# Similarity
-similarity = cosine_similarity(matrix)
-
-# Recommendation function
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = similarity[index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    
-    recommended_movies = []
-    
-    for i in movies_list:
-        recommended_movies.append(movies.iloc[i[0]].title)
-        
-    return recommended_movies
-
-
-# ---------------- UI ---------------- #
+    return [movies.iloc[i[0]].title for i in movies_list]
 
 st.title("🎬 Movie Recommendation System")
 
-selected_movie = st.selectbox(
-    "Choose a movie",
-    movies['title'].values
-)
+selected_movie = st.selectbox("Choose a movie", movies['title'].values)
 
 if st.button("Recommend"):
-    
     recommendations = recommend(selected_movie)
+    cols = st.columns(5)
     
-    st.subheader("Recommended Movies:")
-    
-    for movie in recommendations:
-       poster = fetch_poster(movie)
-       st.image(poster, width=200)
-       st.write(movie)
+    for i, movie in enumerate(recommendations):
+        with cols[i]:
+            poster = fetch_poster(movie)
+            st.image(poster)
+            st.caption(movie)
